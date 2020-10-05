8e82b02d1e08d09d352c74a515b9cd35865f557d
Sébastien Gruet <sebastien-gruet@orange.fr>
2020-10-05 16:57:06 +0200

    Initialized welcome page

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class DefaultController extends AbstractController 
+{
+    /**
+     * @Route("/", name="app_index")
+    */
+    public function index(): Response
+    {
+        return $this->render('wild/index.html.twig', [
+            'title' => 'Bienvenue'
+        ]);
+    }
+}
