aa979d5991604590974f6b7794489e1c4250cfaf
Sébastien Gruet <sebastien-gruet@orange.fr>
2020-10-08 10:46:28 +0200

    Adding dynamic route for a show page, with slug parameter. + test of a link to that page with Twig {{ path() }} expression in index.html.twig

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -6,15 +6,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+* @Route("/wild", name="wild_")
+*/
 Class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild_index")
+     * @Route("/", name="index")
     */
-    public function index() :Response
+    public function index(): Response
     {
         return $this->render('wild/index.html.twig', [
-                'website' => 'Wild Séries',
+                'title' => 'Bienvenue',
+        ]);
+    }
+
+    /**
+     * @Route("/show/{slug}",
+     *  name="show",
+     *  requirements={"slug"="^[a-z0-9-]*$"},
+     *  defaults={"slug"="Aucune série sélectionnée, veuillez choisir une série"}
+     * )
+    */
+    public function show(string $slug): Response
+    {
+        $slug = ucwords(str_replace("-", " ", $slug));
+        return $this->render('wild/show.html.twig', [
+            'slug' => $slug
         ]);
     }
 }
