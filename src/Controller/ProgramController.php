3de56795511881d94e517eac67d9ae09dedb791e
Sébastien Gruet <sebastien-gruet@orange.fr>
2020-10-23 17:35:02 +0200

    First with mailer function and sending fake emails using Mailtrap

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -9,7 +9,12 @@ use App\Service\Slugify;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Twig\Extra\Inky\InkyExtension;
+
 
 /**
  * @Route("/program")
@@ -29,7 +34,7 @@ class ProgramController extends AbstractController
     /**
      * @Route("/new", name="program_new", methods={"GET","POST"})
      */
-    public function new(Request $request, Slugify $slugify): Response
+    public function new(Request $request, Slugify $slugify, MailerInterface $mailer): Response
     {
         $program = new Program();
         $form = $this->createForm(ProgramType::class, $program);
@@ -41,6 +46,18 @@ class ProgramController extends AbstractController
             $program->setSlug($slug);
             $entityManager->persist($program);
             $entityManager->flush();
+
+            $email = (new TemplatedEmail())
+                ->from($this->getParameter('mailer_from'))
+                ->to($this->getParameter('mailer_to'))
+                ->subject('Une nouvelle série vient d\'être publiée !')
+                ->html('<p>Une nouvelle série vient d\'être publiée sur Wild Séries !</p>')
+                ->htmlTemplate('email/notification.html.twig')
+                ->context([
+                    'program' => $program,
+                ]);
+
+            $mailer->send($email);
 
             return $this->redirectToRoute('program_index');
         }
