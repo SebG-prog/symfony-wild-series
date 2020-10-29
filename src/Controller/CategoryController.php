f063d4097ec6b4a20dcd3320382bad2b252bccc8
Sébastien Gruet <sebastien-gruet@orange.fr>
2020-10-29 11:07:06 +0100

    Adding registration with security

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Form\CategoryType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,6 +15,7 @@ class CategoryController extends AbstractController
     /**
      * @param Request
      * @Route("/category/add", name="add_category")
+     * @IsGranted("ROLE_ADMIN")
      * @return Response
      */
     public function add(Request $request): Response
